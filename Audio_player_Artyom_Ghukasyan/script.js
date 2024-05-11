51893969ed73bafadf0071cc34838b49dc454e6e
ArtyomGhukasyan11 <artyom.xukasyan@tumo.org>
2024-05-11 13:28:53 +0400

    data

---
+++ Audio_player_Artyom_Ghukasyan/script.js
@@ -0,0 +1,24 @@
+let data = {
+    title : [
+        "MTG OTIMOS VIDEOGAMES",
+        "Denzel Curry Ultimate",
+        "Minecraft-Theme",
+        "Baile do Coqueiro 5",
+        "THE AUTOTREM",
+        
+    ],
+    song : [
+        "mtg - otimos videogames - laq1zen (sped up + reverb).mp3",
+        "Denzel Curry Ultimate.mp3",
+        "Minecraft-Theme.mp3",
+        "DJ Vilão DS - Baile do Coqueiro 5.mp3",
+        "DJ RICK 013, MC GW - THE AUTOTREM 1.0 (Slowed + Reverb).mp3"
+    ],
+    poster : [
+        "https://source.boomplaymusic.com/group10/M00/10/13/e8300dac9fc84ba6a98e7a51c09e1ca4_320_320.jpg",
+        "https://d-hn-ca-231.dideo.ir/image/a2NjY2JjTFBBSk5rZnYyT1dMWURJZjNocU5EU2xva3paOC9rL2hVd2hoVVJjUTBxRzErUzNJdFB1RTA2TS8wT3k1TmtWMHFEWXVMR08rM25Za0YvVDRiL1BBRVBzd1d1aXFvN3ZxVmtjZGxIUmZqQkd4ZnQ4b05rVjh4UGllcTNTMGgzQmtDWVdpbjNWV29INGxrYWs0OThRWHJWM2RPUmt2RjFnOEd0MHdIRGxTMThNUGw4VmlFUkhDMmxFaFNISGRMYmhJRFYwNTI3R2JlcHFpR21rSENCcFhVeHdweTl2djQyT2hUNnczdE1WN3NIWEJMQTVyVSsyWTN3RE82MjJiSUwwSDlqTXNXWkI2MXhrdVRYak5nK2hUWDZjenViYnM4dEF4TEdyeDhMWTdnZjdid01qcWNVcWpMRXZzZGhKVTZwZW04elVUVVlWNzNnTDJDcEVybFIwYmpGc3k5dEhEYUM0R3BXMXYwPQ==",
+        "https://i.ytimg.com/vi/cjQQ9JYGgTM/maxresdefault.jpg",
+        "https://m.media-amazon.com/images/I/51ALSRurLSL._UXNaN_FMjpg_QL85_.jpg",
+        "https://m.media-amazon.com/images/I/41eJKUJ9uGL._UXNaN_FMjpg_QL85_.jpg"
+    ],
+}
